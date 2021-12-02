bcc1485869323b8973c8bead4c9f3aa03d864b4b
Joseph Cheverton-Wynne <joseph.cheverton-wynne@bjss.com>
2021-12-02 23:41:52 +0000

    day 1 fast refactor

--- src/days/day01.zig
+++ src/days/day01.zig
@@ -17,16 +17,37 @@ pub fn run(contents: []u8, out: anytype) !i128 {
 }
 
 fn solve(contents: []u8, p1: *usize, p2: *usize) !void {
-    var lines = std.mem.tokenize(u8, contents, "\n");
-
     var parsed: [4]usize = undefined;
-
+    var ind: usize = 0;
     var n: usize = 0;
-    while (lines.next()) |line| : (n += 1) {
-        parsed[n % parsed.len] = try std.fmt.parseInt(usize, line, 10);
+    while (ind < contents.len) : (n += 1) {
+        var number: usize = undefined;
+        var size: usize = undefined;
+        toUsize(contents[ind..], &number, &size);
+        parsed[n % parsed.len] = number;
         if (n >= 1 and parsed[n % parsed.len] > parsed[(n - 1) % parsed.len])
             p1.* += 1;
         if (n >= 3 and parsed[n % parsed.len] > parsed[(n - 3) % parsed.len])
             p2.* += 1;
+
+        ind += size + 1;
     }
+}
+
+fn toUsize(contents: []u8, number: *usize, size: *usize) void {
+    var result: usize = 0;
+    var characters: usize = 0;
+
+    for (contents) |char, i| {
+        if ('0' <= char and char <= '9') {
+            result *= 10;
+            result += @as(usize, char - '0');
+            characters = i;
+        } else {
+            break;
+        }
+    }
+
+    number.* = result;
+    size.* = characters + 1;
 }
