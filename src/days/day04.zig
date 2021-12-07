abe384f5094dd0dd5e5749a224fbe7b9f1a9837b
Joseph Cheverton-Wynne <joseph.cheverton-wynne@bjss.com>
2021-12-07 00:12:01 +0000

    day 4 removed pt2 allocs

--- src/days/day04.zig
+++ src/days/day04.zig
@@ -6,13 +6,10 @@ pub fn run(contents: []u8, out: anytype, allocator: *std.mem.Allocator) !i128 {
     var start = std.time.nanoTimestamp();
 
     var state = try loadState(contents, allocator);
-    defer {
-        allocator.free(state.boards);
-        allocator.free(state.called);
-    }
+    defer state.deinit();
 
     var p1: usize = try part1(state);
-    var p2: usize = try part2(state, allocator);
+    var p2: usize = try part2(state);
 
     var duration = std.time.nanoTimestamp() - start;
 
@@ -22,18 +19,17 @@ pub fn run(contents: []u8, out: anytype, allocator: *std.mem.Allocator) !i128 {
 }
 
 fn part1(state: State) !usize {
-    var seen = [_]bool{false} ** 100;
-
     for (state.called) |called| {
-        seen[called] = true;
-        for (state.boards) |board| {
-            if (board.won(seen)) {
+        for (state.boards) |*board| {
+            board.mark(called);
+            if (board.won()) {
                 var result: usize = 0;
 
-                for (board.board) |row| {
-                    for (row) |c| {
-                        if (!seen[c])
-                            result += c;
+                for (board.board[0..5]) |row| {
+                    var i: u7 = 0;
+                    while (i < 100) : (i += 1) {
+                        if (row & (@as(u100, 1) << i) != 0)
+                            result += i;
                     }
                 }
 
@@ -45,81 +41,75 @@ fn part1(state: State) !usize {
     return error.NoWinner;
 }
 
-fn part2(state: State, allocator: *std.mem.Allocator) !usize {
-    var boards = std.ArrayList(*Board).init(allocator);
-    defer boards.deinit();
-    try boards.ensureTotalCapacity(state.boards.len);
-    var swap = std.ArrayList(*Board).init(allocator);
-    defer swap.deinit();
-    try swap.ensureTotalCapacity(state.boards.len);
-
-    for (state.boards) |*board|
-        try boards.append(board);
-
-    var seen = [_]bool{false} ** 100;
-
+fn part2(state: State) !usize {
     for (state.called) |called, i| {
-        swap.clearRetainingCapacity();
+        var winnerInd: usize = 0;
+        var remaining: usize = 0;
+        for (state.boards) |*b, boardInd| {
+            if (b.wonGame) {
+                continue;
+            }
+            b.mark(called);
+            if (!b.won()) {
+                remaining += 1;
+                winnerInd = boardInd;
+            }
+        }
 
-        seen[called] = true;
-        for (boards.items) |b|
-            if (!b.won(seen))
-                try swap.append(b);
-
-        if (swap.items.len == 1) {
+        if (remaining == 1) {
             var ind = i;
-            var lastBoard = swap.items[0];
+            var lastBoard = state.boards[winnerInd];
 
-            while (!lastBoard.won(seen)) : (ind += 1)
-                seen[state.called[ind]] = true;
+            while (!lastBoard.won()) : (ind += 1)
+                lastBoard.mark(state.called[ind]);
 
             var result: usize = 0;
 
-            for (lastBoard.board) |row| {
-                for (row) |c| {
-                    if (!seen[c])
-                        result += c;
+            for (lastBoard.board[0..5]) |row| {
+                var shift: u7 = 0;
+                while (shift < 100) : (shift += 1) {
+                    if (row & (@as(u100, 1) << shift) != 0)
+                        result += shift;
                 }
             }
 
             return result * state.called[ind - 1];
         }
-
-        var temp = boards;
-        boards = swap;
-        swap = temp;
     }
 
     return error.NoSolutionFound;
 }
 
-const State = struct { called: []usize, boards: []Board };
+const State = struct {
+    const Self = @This();
+
+    called: []usize, 
+    boards: []Board,
+    allocator: *std.mem.Allocator,
+
+    fn deinit(self: *Self) void {
+        self.allocator.free(self.called);
+        self.allocator.free(self.boards);
+    }
+ };
 
 const Board = struct {
     const Self = @This();
 
-    board: [5][5]usize,
+    board: [10]u100,
+    wonGame: bool,
 
-    fn won(self: Self, seen: [100]bool) bool {
-        {
-            var row: usize = 0;
-            while (row < 5) : (row += 1) {
-                var col: usize = 0;
-                while (col < 5) : (col += 1) {
-                    if (!seen[self.board[row][col]])
-                        break;
-                } else return true;
-            }
-        }
+    fn mark(self: *Self, val: usize) void {
+        var mask = ~(@as(u100, 1) << @truncate(u7, val));
+        for (self.board) |*b|
+            b.* &= mask;
+    }
 
-        {
-            var col: usize = 0;
-            while (col < 5) : (col += 1) {
-                var row: usize = 0;
-                while (row < 5) : (row += 1) {
-                    if (!seen[self.board[row][col]])
-                        break;
-                } else return true;
+    fn won(self: *Self) bool {
+        for (self.board) |b| {
+            if (b == 0) {
+                self.wonGame = true;
+                return true;
             }
         }
 
@@ -147,25 +137,40 @@ fn loadState(contents: []u8, allocator: *std.mem.Allocator) !State {
     ind += 1;
 
     while (ind < contents.len) {
-        var board: Board = .{ .board = undefined };
+        var board: Board = .{ .board = [_]u100{0} ** 10, .wonGame = false };
         var row: usize = 0;
+        var temp: [5][5]u100 = [_][5]u100{[_]u100{0} ** 5} ** 5;
         while (row < 5) : (row += 1) {
             var cell: usize = 0;
             while (cell < 5) : (cell += 1) {
-                board.board[row][cell] = 0;
+                temp[row][cell] = 0;
                 for (contents[ind .. ind + 2]) |c| {
                     if ('0' <= c and c <= '9') {
-                        board.board[row][cell] *= 10;
-                        board.board[row][cell] += c - '0';
+                        temp[row][cell] *= 10;
+                        temp[row][cell] += c - '0';
                     }
                 }
 
                 ind += 3;
             }
         }
+        for (temp) |tempRow, i| {
+            for (tempRow) |cell| {
+                board.board[i] |= @as(u100, 1) << @truncate(u7, cell);
+            }
+        }
+
+        var column: usize = 0;
+        while (column < 5) : (column += 1) {
+            var tempRow: usize = 0;
+            while (tempRow < 5) : (tempRow += 1) {
+                board.board[column + 5] |= @as(u100, 1) << @truncate(u7, temp[tempRow][column]);
+            }
+        }
+
         try boards.append(board);
         ind += 1;
     }
 
-    return State{ .called = called.toOwnedSlice(), .boards = boards.toOwnedSlice() };
+    return State{ .called = called.toOwnedSlice(), .boards = boards.toOwnedSlice(), .allocator = allocator };
 }
