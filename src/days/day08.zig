1ad0555105dbefe61adf5520dbb155834e1b92bc
Joseph Cheverton-Wynne <joseph.cheverton-wynne@bjss.com>
2021-12-10 00:02:30 +0000

    moved load functions to being methods where appropriate

--- src/days/day08.zig
+++ src/days/day08.zig
@@ -5,7 +5,7 @@ const util = @import("../util.zig");
 pub fn run(contents: []u8, out: anytype, allocator: *std.mem.Allocator) !i128 {
     var start = std.time.nanoTimestamp();
 
-    var entries = try loadEntries(contents, allocator);
+    var entries = try Entry.loadEntries(contents, allocator);
     defer allocator.free(entries);
 
     var p1: usize = part1(entries);
@@ -152,37 +152,37 @@ fn calculateNumber(comptime expectedSize: usize, segments: [7]?u8, expectedSegme
 const Entry = struct {
     patterns: [10][]u8,
     outputs: [4][]u8,
-};
 
-fn loadEntries(contents: []u8, allocator: *std.mem.Allocator) ![]Entry {
-    var entries = std.ArrayList(Entry).init(allocator);
-    errdefer entries.deinit();
+    fn loadEntries(contents: []u8, allocator: *std.mem.Allocator) ![]Entry {
+        var entries = std.ArrayList(Entry).init(allocator);
+        errdefer entries.deinit();
 
-    var ind: usize = 0;
-    while (ind < contents.len) {
-        var entry = Entry{ .patterns = undefined, .outputs = undefined };
-        for (entry.patterns) |*pattern| {
-            var pat = getString(contents[ind..]);
-            for (pat) |*p|
-                p.* -= 'a';
-            std.sort.sort(u8, pat, {}, comptime std.sort.asc(u8));
-            pattern.* = pat;
-            ind += 1 + pattern.*.len;
+        var ind: usize = 0;
+        while (ind < contents.len) {
+            var entry = Entry{ .patterns = undefined, .outputs = undefined };
+            for (entry.patterns) |*pattern| {
+                var pat = getString(contents[ind..]);
+                for (pat) |*p|
+                    p.* -= 'a';
+                std.sort.sort(u8, pat, {}, comptime std.sort.asc(u8));
+                pattern.* = pat;
+                ind += 1 + pattern.*.len;
+            }
+            ind += 2;
+            for (entry.outputs) |*output| {
+                var out = getString(contents[ind..]);
+                for (out) |*o|
+                    o.* -= 'a';
+                std.sort.sort(u8, out, {}, comptime std.sort.asc(u8));
+                output.* = out;
+                ind += 1 + output.*.len;
+            }
+            try entries.append(entry);
         }
-        ind += 2;
-        for (entry.outputs) |*output| {
-            var out = getString(contents[ind..]);
-            for (out) |*o|
-                o.* -= 'a';
-            std.sort.sort(u8, out, {}, comptime std.sort.asc(u8));
-            output.* = out;
-            ind += 1 + output.*.len;
-        }
-        try entries.append(entry);
+
+        return entries.toOwnedSlice();
     }
-
-    return entries.toOwnedSlice();
-}
+};
 
 fn getString(contents: []u8) []u8 {
     var length: usize = 0;
