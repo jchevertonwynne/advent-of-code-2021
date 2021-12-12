b8add54c3d0ac50dabd6b624bc6e5d7a33ef6944
Joseph Cheverton-Wynne <joseph.cheverton-wynne@bjss.com>
2021-12-12 15:20:44 +0000

    load into array instead of hashmap

--- src/days/day12.zig
+++ src/days/day12.zig
@@ -6,11 +6,9 @@ pub fn run(contents: []u8, out: anytype, allocator: *std.mem.Allocator) !i128 {
     var start = std.time.nanoTimestamp();
 
     var caves = try loadCaves(contents, allocator);
-    defer {
-        var it = caves.valueIterator();
-        while (it.next()) |v|
-            v.deinit();
-        caves.deinit();
+    errdefer {
+        for (caves) |*c|
+            c.deinit();
     }
 
     var p1: usize = try part1(caves);
@@ -23,14 +21,14 @@ pub fn run(contents: []u8, out: anytype, allocator: *std.mem.Allocator) !i128 {
     return duration;
 }
 
-fn part1(caves: std.StringHashMap(std.ArrayList([]const u8))) !usize {
+fn part1(caves: [2706]std.ArrayList([]const u8)) !usize {
     var start = "start";
     var history = std.mem.zeroes([2704]bool);
 
     return try explore(.part1, start, &history, null, caves);
 }
 
-fn part2(caves: std.StringHashMap(std.ArrayList([]const u8))) !usize {
+fn part2(caves: [2706]std.ArrayList([]const u8)) !usize {
     var start = "start";
     var history = std.mem.zeroes([2704]bool);
 
@@ -39,10 +37,10 @@ fn part2(caves: std.StringHashMap(std.ArrayList([]const u8))) !usize {
 
 const Part = enum { part1, part2 };
 
-fn explore(comptime part: Part, current: []const u8, history: *[2704]bool, _doubleVisited: ?[]const u8, caves: std.StringHashMap(std.ArrayList([]const u8))) anyerror!usize {
+fn explore(comptime part: Part, current: []const u8, history: *[2704]bool, _doubleVisited: ?[]const u8, caves: [2706]std.ArrayList([]const u8)) anyerror!usize {
     var res: usize = 0;
 
-    for ((caves.get(current) orelse unreachable).items) |option| {
+    for (caves[caveToIndex(current)].items) |option| {
         if (std.mem.eql(u8, "start", option))
             continue;
 
@@ -81,9 +79,8 @@ fn explore(comptime part: Part, current: []const u8, history: *[2704]bool, _doub
 
     if (_doubleVisited) |d| {
         if (!std.mem.eql(u8, current, d))
-         history[caveToIndex(current)] = false;
-    }
-    else {
+            history[caveToIndex(current)] = false;
+    } else {
         history[caveToIndex(current)] = false;
     }
 
@@ -91,6 +88,10 @@ fn explore(comptime part: Part, current: []const u8, history: *[2704]bool, _doub
 }
 
 fn caveToIndex(cave: []const u8) usize {
+    if (std.mem.eql(u8, cave, "start"))
+        return 2704;
+    if (std.mem.eql(u8, cave, "end"))
+        return 2705;
     return if (isSmallCave(cave))
         @as(usize, cave[0] - 'a') * 52 + cave[1]
     else
@@ -101,13 +102,13 @@ fn isSmallCave(cave: []const u8) bool {
     return 'a' <= cave[0] and cave[0] <= 'z';
 }
 
-fn loadCaves(contents: []u8, allocator: *std.mem.Allocator) !std.StringHashMap(std.ArrayList([]const u8)) {
-    var result = std.StringHashMap(std.ArrayList([]const u8)).init(allocator);
+fn loadCaves(contents: []u8, allocator: *std.mem.Allocator) ![2706]std.ArrayList([]const u8) {
+    var result: [2706]std.ArrayList([]const u8) = undefined;
+    for (result) |*r|
+        r.* = std.ArrayList([]const u8).init(allocator);
     errdefer {
-        var it = result.valueIterator();
-        while (it.next()) |v|
-            v.*.deinit();
-        result.deinit();
+        for (result) |*r|
+            r.deinit();
     }
 
     var ind: usize = 0;
@@ -118,15 +119,8 @@ fn loadCaves(contents: []u8, allocator: *std.mem.Allocator) !std.StringHashMap(s
         var a = contents[ind .. ind + hyphen];
         var b = contents[ind + hyphen + 1 .. ind + newline];
 
-        var aEntry = try result.getOrPut(a);
-        if (!aEntry.found_existing)
-            aEntry.value_ptr.* = std.ArrayList([]const u8).init(allocator);
-        try aEntry.value_ptr.*.append(b);
-
-        var bEntry = try result.getOrPut(b);
-        if (!bEntry.found_existing)
-            bEntry.value_ptr.* = std.ArrayList([]const u8).init(allocator);
-        try bEntry.value_ptr.*.append(a);
+        try result[caveToIndex(a)].append(b);
+        try result[caveToIndex(b)].append(a);
 
         ind += newline + 1;
     }
